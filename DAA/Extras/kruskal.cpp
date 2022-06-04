c2476b004419b47e1d27e5efd45344dc07a79b8d
kingpin-ay <ayushmondal001@gmail.com>
2022-06-04 21:09:14 +0530

    added the file for kruskal algo and dbms assignment 4 part B uploaded

---
+++ DAA/Extras/kruskal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define numberOfNode 5
+
+using namespace std;
+
+int main()
+{
+
+    // making array for the node paths and weight values
+    // if we wanted to change the number of node value than we have to do it from the preprocesser
+    int nodeLinkArray[numberOfNode][numberOfNode];
+
+    return 0;
+}
